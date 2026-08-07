shadowlmd/skMHL-avs

commit cd1c4315766e9f4f9b9748dc92ee227e35e4487c

Also extract From address from Origin line.

sources/skmhljam.pas
@@ -942,7 +942,7 @@ function TJamMessageBase.WriteMessage: Boolean;
     if Copy(S, 1, 5) = #1'INTL' then else
     if Copy(S, 1, 5) = #1'FMPT' then else
     if Copy(S, 1, 5) = #1'TOPT' then else
-    if Copy(S, 1, 1) = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
+    if S[1] = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
      begin
       Buffer^.Write(Line^, LenASCIIZ(Line));
 

sources/skcommon.pas
@@ -394,6 +394,7 @@ function LongToStr(const Number: Longint): String;
 function StrToInteger(const S: String; var I: System.Integer): Boolean;
 procedure StrToWord(const S: String; var I: Word);
 function ExtractWord(N: Byte; const S: String; WordDelims: TCharSet): String;
+function CharRPos(C: Char; const S: String): Byte;
 
 procedure ToASCIIZ(const Source: String; const Destination: Pointer);
 function FromASCIIZ(const Source: Pointer): String;
@@ -752,6 +753,19 @@ function ExtractWord(N: Byte; const S: String; WordDelims: TCharSet): String;
       Inc(I);
      end;
    end;
+ end;
+
+function CharRPos(C: Char; const S: String): Byte;
+ var
+  I: Byte;
+ begin
+  for I:=Length(S) downto 1 do
+   if S[I] = C then
+    begin
+     CharRPos:=I;
+     Exit;
+    end;
+  CharRPos:=0;
  end;
 
 procedure ToASCIIZ(const Source: String; const Destination: Pointer);

sources/skmhl.pas
@@ -491,7 +491,7 @@ procedure TMessageBase.GetFromAddress(var Address: TAddress);
    begin
     GetString(S);
 
-    if ((Length(S) = 0) or (S[1] <> #1)) and not First then
+    if ((Length(S) = 0) or ((S[1] <> #1) and (S[2] <> '*'))) and not First then
      Continue;
 
     First:=False;
@@ -990,9 +990,10 @@ function TMessageBase.GetFlag(const Flag: Longint): Boolean;
 procedure TMessageBase.CheckFromAddress(const S: String; var Address: TAddress);
  var
   A: TAddress;
+  B1, B2: Byte;
  begin
-  if Copy(S, 1, 7) = #1'MSGID:' then StrToAddress(ExtractWord(2, S, [' ']), Address);
-  if Copy(S, 1, 5) = #1'INTL'   then
+  if Copy(S, 1, 7) = #1'MSGID:' then StrToAddress(ExtractWord(2, S, [' ']), Address) else
+  if Copy(S, 1, 5) = #1'INTL' then
    begin
     A.Point:=Address.Point;
 
@@ -1000,7 +1001,15 @@ procedure TMessageBase.CheckFromAddress(const S: String; var Address: TAddress);
 
     Address.Point:=A.Point;
    end else
-  if Copy(S, 1, 5) = #1'FMPT'   then StrToInteger(ExtractWord(2, S, [' ']), Address.Point);
+  if Copy(S, 1, 5) = #1'FMPT' then StrToInteger(ExtractWord(2, S, [' ']), Address.Point) else
+  if Copy(S, 1, 10) = ' * Origin:' then
+   begin
+    B1:=CharRPos('(', S);
+    B2:=CharRPos(')', S);
+
+    if (B1 <> 0) and (B2 > B1) then
+     StrToAddress(Copy(S, B1 + 1, B2 - B1 - 1), Address);
+   end;
  end;
 
 procedure TMessageBase.CheckToAddress(const S: String; var Address: TAddress);

commit 4c916d517e7bbfa9d46bb118a7f4b789def71da5

Fix bug introduced in cd1c4315766e9f4f9b9748dc92ee227e35e4487c - accesing String by index returns a value even if String length is zero, this caused empty jamUnknown kludges appearing instead of newlines after kludge lines

sources/skmhljam.pas
@@ -988,7 +988,7 @@ function TJamMessageBase.WriteMessage: Boolean;
     if Copy(S, 1, 5) = #1'INTL' then else
     if Copy(S, 1, 5) = #1'FMPT' then else
     if Copy(S, 1, 5) = #1'TOPT' then else
-    if S[1] = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
+    if Copy(S, 1, 1) = #1 then AddSubField(jamUnknown, Copy(S, 2, 255)) else
      begin
       Buffer^.Write(Line^, LenASCIIZ(Line));
 